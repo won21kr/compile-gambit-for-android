160baa56c6f9d0a5d52444379cbc3c58f872e8d9
tcleval <tcleval@gmail.com>
2013-07-30 11:09:50 -0300

    added parameters for compiler optimization and source code downloading

--- build-gambit.sh
+++ build-gambit.sh
@@ -1,27 +1,50 @@
 
-
+####################################################################################
+# build-gambit.sh                                                                  #
+####################################################################################
 # Script to build gambit-c as a static libray for android
 # 
-# copyright?: Daniel de Matos Alves
+# copyright: Daniel de Matos Alves
+#            free beer licence.
+#            You are free to use this code the way you want.
+# HOW TO USE IT:
+#           Change parameters bellow and execute the script.
 #
-
-# gambit-c version
-MY_VERSION=4.7.0
-MY_ANDROID_ROOT=/home/daniel/programas/android-ndk/
-
+#
+# TODO: give user choice of android ndk, for now I am using r8
+#
 # THIS script replace 2 files while building gambit because they are TOO OLD:
 #
 #     cp /usr/share/libtool/config/config.sub .
 #     cp /usr/share/libtool/config/config.guess .
 # if you are using Linux, these files should be on your system.
 
+##############PARAMETERS#############################################################
+# Change values to your needs                                                       #
+####################################################################################
+# gambit-c version
+MY_VERSION=4.7.0
+# android ndk home directory
+MY_ANDROID_ROOT=/home/daniel/programas/android-ndk/
+# comment this flag if don't want gambit to be build fully optimized
+MY_OPTIMIZE=YES
+# comment the flag bellow if you don't want gambit source code to be downloaded
+MY_DOWNLOAD=YES
+##############PARAMETERS-END#########################################################
+
+#####################################
+# NO NEED TO CHANGE BELLOW THIS LINE#
+#####################################
+#------------------------------------------------------------------------------------
+
+# set gambit source code url
+GAMBIT_BASENAME=gambc-v${MY_VERSION//\./_}
+URL=`echo $MY_VERSION| awk 'BEGIN { FS="." }  { print $1"."$2 }'`
+URL="http://www.iro.umontreal.ca/~gambit/download/gambit/v"$URL"/source"
+URL=${URL}/${GAMBIT_BASENAME}.tgz
 
 
-# NO NEED TO CHANGE BELLOW THIS LINE ------------------------------------------------------------
-
-GAMBIT_FILE=gambc-v${MY_VERSION//\./_}
-
-# all targets
+# ANDROID ARCHITECTURES
 MY_TARGETS=(arm-linux-androideabi mipsel-linux-android i686-android-linux)
 MY_ROOT_DIR=`pwd`
 MY_BUILD_DIR=${MY_ROOT_DIR}/build_gambit/
@@ -32,6 +55,12 @@ MY_DROID_INCLUDES=($MY_ANDROID_ROOT/platforms/android-8/arch-arm/usr/include/ \
 MY_DROID_LIBS=($MY_ANDROID_ROOT/platforms/android-8/arch-arm/usr/lib/ \
                $MY_ANDROID_ROOT/platforms/android-9/arch-mips/usr/lib/ \
 	       $MY_ANDROID_ROOT/platforms/android-9/arch-x86/usr/lib/ )
+
+# if optimization is set - change the configure script flags
+CFGOPTS=""
+if [ "$MY_OPTIMIZE" ] ; then
+   CFGOPTS="$CFGOPTS  --enable-single-host --enable-c-opt --enable-gcc-opts"
+fi
 
 export_all_paths()
 {
@@ -55,8 +84,8 @@ prepare_for_building()
 unpack_gambit()
 {
     #tar xvfz ${MY_GAMBIT_VERSION}.tgz >>/dev/null 2>&1 
-    tar xvfz ${GAMBIT_FILE}.tgz 
-    cd ${GAMBIT_FILE}
+    tar xvfz ${GAMBIT_BASENAME}.tgz 
+    cd ${GAMBIT_BASENAME}
 
    # gambit config.sub and config.guess ar TOO OLD. I replace here for the ones on my system 
     cp /usr/share/libtool/config/config.sub .
@@ -66,10 +95,15 @@ unpack_gambit()
 remove_gambit()
 {
     cd ${MY_ROOT_DIR}
-    rm -rf ${GAMBIT_FILE}
+    rm -rf ${GAMBIT_BASENAME}
 }
 
-# $1 is the arch
+download_source()
+{
+  wget -c ${URL}
+}
+
+# $1 is the architecture
 copy_gambit_library()
 {
   cp lib/libgambc.a ${MY_BUILD_DIR}/${1}/lib/libgambc-${MY_VERSION}.a
@@ -92,7 +126,7 @@ compile_for_arch()
     export CC=${TARGET}-gcc
     export CXX=${TARGET}-g++
 
-    ./configure  --prefix=${MY_BUILD_DIR}  --target=${TARGET} --host=${TARGET}  CC=${CC} CPPFLAGS="-DANDROID -I$INCLUDES -fno-short-enums" --enable-single-host --enable-c-opt --enable-gcc-opts  CFLAGS="-DANDROID -fno-short-enums -I$INCLUDES -nostdlib -fPIC" LDFLAGS="-Wl,-rpath-link=$LIBS -L$LIBS" LIBS="-lc -ldl" >>${LOG_FILE} 2>&1
+    ./configure  --prefix=${MY_BUILD_DIR}  --target=${TARGET} --host=${TARGET}  CC=${CC} CPPFLAGS="-DANDROID -I$INCLUDES -fno-short-enums" ${CFGOPTS} CFLAGS="-DANDROID -fno-short-enums -I$INCLUDES -nostdlib -fPIC" LDFLAGS="-Wl,-rpath-link=$LIBS -L$LIBS" LIBS="-lc -ldl" >>${LOG_FILE} 2>&1
     cd lib
     make  >>${LOG_FILE} 2>&1
     cd ..
@@ -101,7 +135,15 @@ compile_for_arch()
 compile_everything()
 {
     export_all_paths
+
     rm -rf ${MY_BUILD_DIR}
+
+    if [ "$MY_DOWNLOAD" ] ; then
+        download_source
+    fi
+
+    # build for every architecture
+
     for i in 0 1 2; do
         prepare_for_building ${MY_TARGETS[i]} 
 	echo "unpacking gambit  ... "
